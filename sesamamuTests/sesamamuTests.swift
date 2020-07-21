45d5a1fb893ba50847a3cd8adc96616769243096
Baskoro Indrayana <baskoroindrayana@gmail.com>
2020-07-21 15:26:02 +0700

    Initial commit

---
+++ sesamamuTests/sesamamuTests.swift
@@ -0,0 +1,34 @@
+//
+//  sesamamuTests.swift
+//  sesamamuTests
+//
+//  Created by Baskoro Indrayana on 07/21/20.
+//  Copyright © 2020 Baskoro Indrayana. All rights reserved.
+//
+
+import XCTest
+@testable import sesamamu
+
+class sesamamuTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
