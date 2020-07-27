f3ca32abaa39becb30e818238714901313f3c4ac
Davia Belinda <daviabelinda@gmail.com>
2020-07-27 09:59:38 +0700

    add HomeView, AvatarView, and assets

---
+++ sesamamu/CustomTextField.swift
@@ -0,0 +1,50 @@
+//
+//  CustomTextField.swift
+//  swiftUIPractice
+//
+//  Created by Davia Belinda Hidayat on 21/07/20.
+//  Copyright © 2020 Davia Belinda Hidayat. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+struct CustomTextField: UIViewRepresentable {
+
+    class Coordinator: NSObject, UITextFieldDelegate {
+
+        @Binding var text: String
+        var didBecomeFirstResponder = false
+
+        init(text: Binding<String>) {
+            _text = text
+        }
+
+        func textFieldDidChangeSelection(_ textField: UITextField) {
+            text = textField.text ?? ""
+        }
+
+    }
+
+    @Binding var text: String
+    var isFirstResponder: Bool = false
+
+    func makeUIView(context: UIViewRepresentableContext<CustomTextField>) -> UITextField {
+        let textField = UITextField(frame: .zero)
+        textField.delegate = context.coordinator
+        return textField
+    }
+
+    func makeCoordinator() -> CustomTextField.Coordinator {
+        return Coordinator(text: $text)
+    }
+
+    func updateUIView(_ uiView: UITextField, context: UIViewRepresentableContext<CustomTextField>) {
+        uiView.text = text
+        if isFirstResponder && !context.coordinator.didBecomeFirstResponder  {
+            uiView.becomeFirstResponder()
+            context.coordinator.didBecomeFirstResponder = true
+        }
+    }
+}
