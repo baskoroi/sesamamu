8ac8ae3566b106ba36ce216fef56affac465492e
Baskoro Indrayana <baskoroindrayana@gmail.com>
2020-07-29 11:12:28 +0700

    add view to show all answers from a question, reorg fonts + add FontSelector struct

---
+++ sesamamu/ViewModels/AnswerViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  AnswerViewModel.swift
+//  sesamamu
+//
+//  Created by Baskoro Indrayana on 07/21/20.
+//  Copyright © 2020 Baskoro Indrayana. All rights reserved.
+//
+
+import SwiftUI
+
+struct AnswerViewModel: Hashable {
+    var id = UUID()
+
+    var nickname: String?
+    var message: String?
+    var isMyOwn: Bool = false
+    var avatarURL: String?
+    var timestamp: TimeInterval
+    var foregroundColor: Color = .white
+    var backgroundColor: Color = .blue
+}
