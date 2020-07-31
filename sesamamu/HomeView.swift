20a45be07815a5e020388c49032a6087bc434c92
Davia Belinda <daviabelinda@gmail.com>
2020-07-31 22:12:29 +0700

    changes in avatar view and home view, keyboard fixed

--- sesamamu/HomeView.swift
+++ sesamamu/HomeView.swift
@@ -18,6 +18,10 @@ struct HomeView: View {
     
     @State private var keyboardHeight: CGFloat = 0
     
+    @State var isNavigationActive = false
+    
+    @ObservedObject var keyboardResponder = KeyboardResponder()
+    
     let lightBlue = Color(red: 177.0/255.0, green: 224.0/255.0, blue: 232.0/255.0, opacity: 1.0)
     
     
@@ -25,7 +29,7 @@ struct HomeView: View {
     var body: some View {
         NavigationView {
             GeometryReader { geometry in
-                ZStack(alignment: .center){
+                ZStack(alignment: .center) {
                     Image("backgroundhome2")
                         .resizable()
                         .aspectRatio(contentMode: .fill)
@@ -65,16 +69,27 @@ struct HomeView: View {
                                     .background(self.lightBlue)
                                     .cornerRadius(12)
                                     .onTapGesture {
-                                }
+                                        self.isRoomIDFieldActive = true
+                                        if self.isRoomIDFieldActive{
+                                            self.value = self.keyboardHeight
+                                        }else{
+                                            self.value = 0
+                                            self.isRoomIDFieldActive = false
+                                        }
+                                    }
                                 
-                                NavigationLink(destination: AvatarView(host: false)){
+                                NavigationLink(destination: AvatarView(host: false), isActive: self.$isNavigationActive){
                                     Image("rightButton")
                                         .renderingMode(.original)
                                         .resizable()
                                         .aspectRatio(contentMode: .fit)
                                         .frame(width: 30)
-                                }
-                            }.offset(y: -160)
+                                        
+                                }.navigationBarBackButtonHidden(false)
+                                .navigationBarHidden(false)
+                                
+                            }.offset(y: self.isRoomIDFieldActive ? -200:0)
+                            
                         }
                         
                         if !self.isRoomIDFieldActive && self.roomID.isEmpty {
@@ -98,27 +113,55 @@ struct HomeView: View {
                                     .aspectRatio(contentMode: .fit)
                                     .frame(width: 250, alignment: .center)
                                     .padding(.bottom, UIScreen.main.bounds.height - (UIScreen.main.bounds.height-175))
-                            }
+                            }.simultaneousGesture(TapGesture().onEnded{
+                                
+                            })
                         }
-                        
-                    }.offset(y: self.value)
-                        .animation(.spring())
-                        .keyboardAdaptive(onAppear: {
-                            self.value = self.keyboardHeight
-                            self.isRoomIDFieldActive = true
-                        }, onHide: {
-                            self.value = 0
-                            self.isRoomIDFieldActive = false
-                        })
+                    }.KeyboardAwarePadding()
+                    .animation(.spring())
+                    
+                    //.animation(.spring())
+                    //.offset(y: -self.keyboardResponder.currentHeight*0.9)
                     
                 }
             }.onTapGesture {
+                self.isRoomIDFieldActive = false
                 self.hideKeyboard()
             }.onAppear {
                 print("MUNCUL LAGI")
             }
-            
-        }
+        
+            }
+        .navigationBarTitle("")
+        .navigationBarHidden(isNavigationActive)
+    }
+}
+
+struct KeyboardAwareModifier: ViewModifier {
+    @State private var keyboardHeight: CGFloat = 0
+
+    private var keyboardHeightPublisher: AnyPublisher<CGFloat, Never> {
+        Publishers.Merge(
+            NotificationCenter.default
+                .publisher(for: UIResponder.keyboardWillShowNotification)
+                .compactMap { $0.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue }
+                .map { $0.cgRectValue.height },
+            NotificationCenter.default
+                .publisher(for: UIResponder.keyboardWillHideNotification)
+                .map { _ in CGFloat(0) }
+       ).eraseToAnyPublisher()
+    }
+
+    func body(content: Content) -> some View {
+        content
+            .padding(.bottom, keyboardHeight)
+            .onReceive(keyboardHeightPublisher) { self.keyboardHeight = $0 }
+    }
+}
+
+extension View {
+    func KeyboardAwarePadding() -> some View {
+        ModifiedContent(content: self, modifier: KeyboardAwareModifier())
     }
 }
 
@@ -136,91 +179,4 @@ extension View {
 }
 #endif
 
-extension Publishers {
-    // 1.
-    static var keyboardHeight: AnyPublisher<CGFloat, Never> {
-        // 2.
-        let willShow = NotificationCenter.default.publisher(for: UIApplication.keyboardWillShowNotification)
-            .map { $0.keyboardHeight }
-        
-        let willHide = NotificationCenter.default.publisher(for: UIApplication.keyboardWillHideNotification)
-            .map { _ in CGFloat(0) }
-        
-        // 3.
-        return MergeMany(willShow, willHide)
-            .eraseToAnyPublisher()
-    }
-}
 
-extension Notification {
-    var keyboardHeight: CGFloat {
-        return (userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect)?.height ?? 0
-    }
-}
-
-struct KeyboardAdaptive: ViewModifier {
-    @State private var bottomPadding: CGFloat = 0
-    
-    var onAppear: (()->())?
-    var onHide: (()->())?
-    
-    
-    func body(content: Content) -> some View {
-        // 1.
-        GeometryReader { geometry in
-            content
-                .padding(.bottom, self.bottomPadding)
-                // 2.
-                .onReceive(Publishers.keyboardHeight) { keyboardHeight in
-                    // 3.
-                    //                    let keyboardTop = geometry.frame(in: .global).height - keyboardHeight
-                    //                    // 4.
-                    //                    let focusedTextInputBottom = UIResponder.currentFirstResponder?.globalFrame?.maxY ?? 0
-                    //                    // 5.
-                    //                    self.bottomPadding = max(0, focusedTextInputBottom - keyboardTop - geometry.safeAreaInsets.bottom)
-                    
-                    print("\(keyboardHeight)")
-                    
-                    self.bottomPadding = keyboardHeight
-                    
-                    if keyboardHeight != 0 && self.onAppear != nil {
-                        print("masukOnAppear")
-                        self.onAppear!()
-                    }
-                    if keyboardHeight == 0 && self.onHide != nil {
-                        print("masukOnHide")
-                        self.onHide!()
-                    }
-                    
-            }
-                // 6.
-                .animation(.easeOut(duration: 0.16))
-        }
-    }
-    
-}
-
-extension View {
-    func keyboardAdaptive(onAppear: ( () -> () )? = nil, onHide: ( ()->() )? = nil ) -> some View {
-        ModifiedContent(content: self, modifier: KeyboardAdaptive(onAppear: onAppear, onHide: onHide))
-    }
-}
-
-extension UIResponder {
-    static var currentFirstResponder: UIResponder? {
-        _currentFirstResponder = nil
-        UIApplication.shared.sendAction(#selector(UIResponder.findFirstResponder(_:)), to: nil, from: nil, for: nil)
-        return _currentFirstResponder
-    }
-    
-    private static weak var _currentFirstResponder: UIResponder?
-    
-    @objc private func findFirstResponder(_ sender: Any) {
-        UIResponder._currentFirstResponder = self
-    }
-    
-    var globalFrame: CGRect? {
-        guard let view = self as? UIView else { return nil }
-        return view.superview?.convert(view.frame, to: nil)
-    }
-}
